ee8e49828dc3a755e4602b48bf7c48a7df3782e6
RaykkonerD <raykonerd@gmail.com>
2025-03-04 01:17:30 -0300

    feat: criação de mappers para Customer e Trip

---
+++ src/main/java/br/edu/ifpb/hopin_daw2/mappers/CustomerMapper.java
@@ -0,0 +1,29 @@
+package br.edu.ifpb.hopin_daw2.mappers;
+
+import br.edu.ifpb.hopin_daw2.api.dto.CustomerResponseDTO;
+import br.edu.ifpb.hopin_daw2.core.domain.customer.Customer;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomerMapper {
+    public CustomerResponseDTO toDTO(Customer customer) {
+        return new CustomerResponseDTO(
+                customer.getId(),
+                customer.getName(),
+                customer.getEmail(),
+                customer.getPassword(),
+                customer.getCreatedAt()
+        );
+    }
+
+    public Customer toEntity(CustomerResponseDTO dto) {
+        Customer customer = new Customer();
+        customer.setId(dto.id());
+        customer.setName(dto.name());
+        customer.setEmail(dto.email());
+        customer.setPassword(dto.password());
+        customer.setCreatedAt(dto.createdAt());
+
+        return customer;
+    }
+}
---
+++ src/main/java/br/edu/ifpb/hopin_daw2/mappers/TripMapper.java
@@ -0,0 +1,36 @@
+package br.edu.ifpb.hopin_daw2.mappers;
+
+import br.edu.ifpb.hopin_daw2.api.dto.TripResponseDTO;
+import br.edu.ifpb.hopin_daw2.core.domain.trips.Trip;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TripMapper {
+    public TripResponseDTO toDTO(Trip trip) {
+        return new TripResponseDTO(
+                trip.getId(),
+                trip.getCustomer(),
+                trip.getDriver(),
+                trip.getPayment(),
+                trip.getStatus(),
+                trip.getSource(),
+                trip.getDestination(),
+                trip.getCreatedAt()
+        );
+    }
+
+    public Trip toEntity(TripResponseDTO dto) {
+        Trip trip = new Trip();
+
+        trip.setId(dto.id());
+        trip.setCustomer(dto.customer());
+        trip.setDriver(dto.driver());
+        trip.setPayment(dto.payment());
+        trip.setStatus(dto.status());
+        trip.setSource(trip.getSource());
+        trip.setDestination(trip.getDestination());
+        trip.setCreatedAt(trip.getCreatedAt());
+
+        return trip;
+    }
+}
