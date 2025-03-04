2d9f0e85dd9363d39a2e6ff82fde070f98e4d2ef
RaykkonerD <raykonerd@gmail.com>
2025-03-04 01:16:44 -0300

    feat: criação de DTO de response para Trip

---
+++ src/main/java/br/edu/ifpb/hopin_daw2/api/dto/TripResponseDTO.java
@@ -0,0 +1,21 @@
+package br.edu.ifpb.hopin_daw2.api.dto;
+
+import br.edu.ifpb.hopin_daw2.core.domain.customer.Customer;
+import br.edu.ifpb.hopin_daw2.core.domain.driver.Driver;
+import br.edu.ifpb.hopin_daw2.core.domain.payments.Payment;
+import br.edu.ifpb.hopin_daw2.core.domain.trips.Status;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record TripResponseDTO(
+        UUID id,
+        Customer customer,
+        Driver driver,
+        Payment payment,
+        Status status,
+        String origin,
+        String destination,
+        LocalDateTime createdAt
+) {
+}
