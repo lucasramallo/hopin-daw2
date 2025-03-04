9eed1d02f862dc3fe06f9e762ccbcfd4d22dc2d1
RaykkonerD <raykonerd@gmail.com>
2025-03-04 01:18:29 -0300

    feat: criação de endpoint de registro de Customers

--- src/main/java/br/edu/ifpb/hopin_daw2/api/controllers/AuthController.java
+++ src/main/java/br/edu/ifpb/hopin_daw2/api/controllers/AuthController.java
@@ -1,7 +1,10 @@
 package br.edu.ifpb.hopin_daw2.api.controllers;
 
 import br.edu.ifpb.hopin_daw2.api.dto.CreateDriverRequestDTO;
+import br.edu.ifpb.hopin_daw2.api.dto.CustomerRequestDTO;
+import br.edu.ifpb.hopin_daw2.api.dto.CustomerResponseDTO;
 import br.edu.ifpb.hopin_daw2.api.dto.DriverResponseDTO;
+import br.edu.ifpb.hopin_daw2.core.service.CustomerService;
 import br.edu.ifpb.hopin_daw2.core.service.DriverService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -15,12 +18,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 public class AuthController {
     @Autowired
-    private DriverService service;
+    private DriverService driverService;
+
+    @Autowired
+    private CustomerService customerService;
 
     @PostMapping("/driver/register")
     public ResponseEntity<DriverResponseDTO> createDriver(@RequestBody CreateDriverRequestDTO request) {
-        DriverResponseDTO reponse = service.createDriver(request);
+        DriverResponseDTO reponse = driverService.createDriver(request);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(reponse);
+    }
+
+    @PostMapping("/customer/register")
+    public ResponseEntity<CustomerResponseDTO> createCustomer(@RequestBody CustomerRequestDTO request) {
+        CustomerResponseDTO response = customerService.createCustomer(request);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 }
