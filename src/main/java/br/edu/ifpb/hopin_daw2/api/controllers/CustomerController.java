cf9d6745f77e5bd190907e3a3b393b8163fdbdb7
RaykkonerD <raykonerd@gmail.com>
2025-03-04 01:18:56 -0300

    feat: criação de controller para Customer

---
+++ src/main/java/br/edu/ifpb/hopin_daw2/api/controllers/CustomerController.java
@@ -0,0 +1,56 @@
+package br.edu.ifpb.hopin_daw2.api.controllers;
+
+import br.edu.ifpb.hopin_daw2.api.dto.CustomerRequestDTO;
+import br.edu.ifpb.hopin_daw2.api.dto.CustomerResponseDTO;
+import br.edu.ifpb.hopin_daw2.api.dto.SearchDTO;
+import br.edu.ifpb.hopin_daw2.api.dto.TripResponseDTO;
+import br.edu.ifpb.hopin_daw2.core.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/customer")
+public class CustomerController {
+    @Autowired
+    private CustomerService service;
+
+    @GetMapping("/{customerId}")
+    public ResponseEntity<CustomerResponseDTO> getCustomerById(@PathVariable UUID customerId) {
+        CustomerResponseDTO response = service.getCustomerById(customerId);
+
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @GetMapping("/findByEmail")
+    public ResponseEntity<CustomerResponseDTO> getCustomerByEmail(@RequestParam String email) {
+        CustomerResponseDTO response = service.getCustomerByEmail(email);
+
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @GetMapping("/{customerId}/getTripsHistory")
+    public ResponseEntity<Page<TripResponseDTO>> getTripsHistory(@PathVariable UUID customerId, SearchDTO search) {
+        Page<TripResponseDTO> trips = service.getTripsHistory(customerId, search);
+
+        return ResponseEntity.status(HttpStatus.OK).body(trips);
+    }
+
+    @PutMapping("/{customerId}")
+    public ResponseEntity<CustomerResponseDTO> editCustomer(@PathVariable UUID customerId, @RequestBody CustomerRequestDTO request) {
+        CustomerResponseDTO response = service.editCustomer(customerId, request);
+
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @DeleteMapping("/{customerId}")
+    public ResponseEntity<CustomerResponseDTO> deleteCustomer(@PathVariable UUID customerId) {
+        service.deleteCustomer(customerId);
+
+        return ResponseEntity.noContent().build();
+    }
+}
